bad6e4aea4491d25964077715ee3ecfa1ae65ad4
xin zhang <xin_zhang_2000@yahoo.com>
2017-01-08 21:41:04 +1100

    problem13-inprogress

--- problem13.go
+++ problem13.go
@@ -14,25 +14,42 @@ func check(e error) {
 	}
 }
 
-type Grid [20][20]int
+type LargeNumberStr [50]string
+
+func LargeNumberAdd(number1 string, number2 string) string {
+	fmt.Println(number1, "  ", number2)
+	r1 := []rune(number1)
+	r2 := []rune(number2)
+
+	var result [10]int
+	r:=0;
+	for i:=9; i<0; i-- {
+			k,_ := strconv.Atoi(r1[i])
+			m,_ := strconv.Atoi(r2[i])
+			t := k+m+r
+			r=t/10
+			result[i] = t
+	}
+
+ 	return string(result)
+}
 
 func main() {
 	//set up the data
-	var data Grid
+	var data LargeNumberStr
 	f, _ := os.Open("problem13data.txt")
 	scanner := bufio.NewScanner(f)
 	i := 0
+	result := ""
 	for scanner.Scan() {
-		lineStr := scanner.Text()
-
-		vals := strings.Fields(lineStr)
-		for idx := range vals {
-			data[i][idx], _ = strconv.Atoi(vals[idx])
+		LargeNumberStr[i] = scanner.Text()
+		if (i>0){
+			result = LargeNumberAdd(result, LargeNumberStr[i])
+		} else if (i==0) {
+			result = LargeNumberStr[i];
 		}
 		i++
 	}
-
-	result := 0
 
 	fmt.Println(result)
 
