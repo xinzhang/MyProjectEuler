cbf5925e255f1dcb750759d1fed1e8995d97f973
Xin Zhang <Xin.Zhang@ausmaq.com.au>
2017-01-12 17:25:57 +1100

    prob22

--- problem22.go
+++ problem22.go
@@ -11,17 +11,30 @@ func check(e error) {
 	}
 }
 
+func SortStringArray(current []string, val string) []string {
+	result := make([]string, len(current)+1)
+
+}
+
 func main() {
 	data, err := ioutil.ReadFile("p022_names.txt")
 	check(err)
 
-	tmp
+	tmp := make([]byte, 0)
 	cnt := 0
 	for i := 0; i < len(data); i++ {
 		s := string(data[i])
 		if s == "," {
+			//stopped and show the tmp
+			fmt.Println(string(tmp))
+			tmp = make([]byte, 0)
 			cnt++
+		} else if s == "\"" {
+			continue
+		} else {
+			tmp = append(tmp, data[i])
 		}
+
 	}
 
 	fmt.Println(cnt)
